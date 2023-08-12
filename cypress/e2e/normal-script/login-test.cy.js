932eab305f18d1e87efeaa0ef245a7cbc398ff54
Gali998 <galimannage6.gm@gmail.com>
2023-08-12 09:19:31 +0530

    assert inventory url

--- cypress/e2e/normal-script/login-test.cy.js
+++ cypress/e2e/normal-script/login-test.cy.js
@@ -4,5 +4,6 @@ describe('Login Test',function(){
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
+        cy.url().should('contains','/inventory.html')
     })
 })
